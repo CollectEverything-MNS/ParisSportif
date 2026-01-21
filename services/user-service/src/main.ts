a54419ded57bdc4f96c4eb5f65adf34667643bd6
Seynabou BA KANE <seynabou.bk@gmail.com>
2026-01-21 13:41:18 +0100

    even user delete and update

--- services/user-service/src/main.ts
+++ services/user-service/src/main.ts
@@ -1,6 +1,7 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { RmqOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,8 +14,24 @@ async function bootstrap() {
     }),
   );
 
-  const port = Number(process.env.USER_SERVICE_PORT);
+  const url = process.env.RABBITMQ_URL;
+  if (!url) {
+    throw new Error('RABBITMQ_URL is not defined');
+  }
 
+
+  app.connectMicroservice<RmqOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [url],
+      queue: 'auth_events',
+      queueOptions: { durable: true },
+    },
+  });
+
+  await app.startAllMicroservices();
+
+  const port = Number(process.env.USER_SERVICE_PORT);
   if (!port) {
     throw new Error('USER_SERVICE_PORT is not defined');
   }
